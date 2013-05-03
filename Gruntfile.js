aeee524786b29205bd0d7ae2a7a596a1b673053f
Nathan Stilwell <nathanstilwell@gmail.com>
2013-05-03 09:15:46 -0400

    Set up Grunt for a development environment

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      spec: {
+        src: ['spec/**/*.js']
+      }
+    },
+
+    connect: {
+      spec: {
+        options: {
+          port: 9898
+        }
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib']
+      },
+      spec: {
+        files: '<%= jshint.spec.src %>',
+        tasks: ['jshint:spec']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['connect', 'watch']);
+};
