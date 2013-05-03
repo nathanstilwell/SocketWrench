15eb36ae7b51f90ee3783443afe4c6b05430476d
Nathan Stilwell <nathanstilwell@gmail.com>
2013-05-03 11:19:18 -0400

    Adding some tests

---
+++ spec/socketWrenchSpec.js
@@ -0,0 +1,15 @@
+/*global describe: false, it: false, expect: false, waits: false, beforeEach: false, runs: false */
+/*global loadFixtures: false, $: false, spyOn: false, define: false */
+
+describe('Socket Wrench', function () {
+  beforeEach(function () {
+    this.wrench = new SocketWrench({
+      socketUrl : 'ws://localhost:4014',
+      autoConnect : false
+    });
+  });// beforeEach
+
+  it('should be defined', function () {
+    expect(typeof this.wrench).toBe('object');
+  });
+}); // describe Socket Wrench
